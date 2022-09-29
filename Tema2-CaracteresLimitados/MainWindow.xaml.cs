bb6cd0d8ecda5da8410bc6234c934c9a6fe9eeed
JOSÉ ALFREDO ZARAGOZA MARCO <joszarmar@alu.edu.gva.es>
2022-09-29 12:55:47 +0200

    Agregar archivos de proyecto.

---
+++ Tema2-CaracteresLimitados/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema2_CaracteresLimitados
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int MAXIMO_CARACTERES = 140;
+        private int numeroCaracteres = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            contadorTextBlock.Text = this.numeroCaracteres + contadorTextBlock.Text;
+        }
+
+        private void inputChanged(object sender, TextChangedEventArgs e)
+        {
+            this.numeroCaracteres = inputTextBox.Text.Length;
+
+            if(inputTextBox.Text.Length < MAXIMO_CARACTERES)
+            {
+                contadorTextBlock.Text = "/140";
+                contadorTextBlock.Text = this.numeroCaracteres + contadorTextBlock.Text;
+            }
+            else if(inputTextBox.Text.Length >= MAXIMO_CARACTERES)
+            {
+                contadorTextBlock.Text = "/140";
+                contadorTextBlock.Text = this.numeroCaracteres + contadorTextBlock.Text;
+                inputTextBox.IsReadOnly = true;
+            }
+        }
+
+        private void IsEnter(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                inputTextBox.Text += "\n";
+                //inputTextBox.Cursor
+            }
+        }
+    }
+}
